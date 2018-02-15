2e06317d879bfbc627cf41b82451b5f77f21f06a
ktse18 <36411157+ktse18@users.noreply.github.com>
2018-02-15 09:37:33 -0500

    Add files via upload

---
+++ 06/ALetterToYourself.java
@@ -0,0 +1,16 @@
+public class ALetterToYourself
+{
+	public static void main(String[] args)
+	{
+		System.out.println("+----------------------------------+");
+		System.out.println("|                              ####|");
+		System.out.println("|                              ####|");
+		System.out.println("|                              ####|");
+		System.out.println("|                                  |");
+		System.out.println("|               Kelvin Tse         |");
+		System.out.println("|               1 Microsoft Way    |");
+		System.out.println("|               Redmond, WA 98104  |");
+		System.out.println("|                                  |");
+		System.out.println("+----------------------------------+");
+	}
+}
---
+++ 07/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials
+{
+	public static void main(String[] args)
+	{
+		System.out.println("K   K  TTTTT");  
+		System.out.println("K  K     T");
+		System.out.println("K K      T");
+		System.out.println("KK       T");
+		System.out.println("K K      T");
+		System.out.println("K  K     T");
+		System.out.println("K   K    T");
+	}
+}
